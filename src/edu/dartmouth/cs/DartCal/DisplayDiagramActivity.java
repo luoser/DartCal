da4a055831108d0ec13d12e3bdacb1cf0320183a
luolisa <lisa@dali.dartmouth.edu>
2014-05-28 11:27:33 -0400

    removed up button from display diagram that crashed the app

--- src/edu/dartmouth/cs/DartCal/DisplayDiagramActivity.java
+++ src/edu/dartmouth/cs/DartCal/DisplayDiagramActivity.java
@@ -14,32 +14,33 @@ public class DisplayDiagramActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_display_diagram);
+		getActionBar().setDisplayHomeAsUpEnabled(false);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.display_diagram, menu);
-		return true;
-	}
+	// @Override
+	// public boolean onCreateOptionsMenu(Menu menu) {
+	//
+	// // Inflate the menu; this adds items to the action bar if it is present.
+	// getMenuInflater().inflate(R.menu.display_diagram, menu);
+	// return true;
+	// }
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		// Handle action bar item clicks here. The action bar will
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
+		// int id = item.getItemId();
+		// if (id == R.id.action_settings) {
+		// return true;
+		// }
 		return super.onOptionsItemSelected(item);
 	}
-	
-	public void consultORC(View view){
+
+	public void consultORC(View view) {
 		Intent launchBrowser = new Intent(android.content.Intent.ACTION_VIEW);
 		launchBrowser.setData(Uri.parse("http://www.dartmouth.edu/~reg/"));
-        startActivity(launchBrowser);
+		startActivity(launchBrowser);
 	}
 
 }
